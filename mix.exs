57f06c7d21a8bf6644fb6d37402348e179ae2c8c
Simeon Manolov <simeon.manolov@sumup.com>
2021-09-24 13:39:27 +0300

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsSigner.MixProject do
   def project do
     [
       app: :aws_signer,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
